3703dd5017b8794e3e35c6dc0cb7b14ac5783a6c
rahul-nile <rahul.katre@niletechnologies.com>
2023-11-28 15:43:42 +0530

    Added timesheet records

---
+++ app/Models/ServiceTimesheet.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ServiceTimesheet extends Model
+{
+    use HasFactory;
+    protected $table = 'service_timesheet';
+    protected $key = 'id';
+    public $timestamps = false;
+}
